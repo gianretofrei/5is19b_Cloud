5e179b22ac1a522189808547a0e5cc18b987ce86
gianretofrei <86076220+gianretofrei@users.noreply.github.com>
2022-06-22 11:57:47 +0200

    Update install_BOINC_2.sh

--- install_BOINC_2.sh
+++ install_BOINC_2.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd ~
 sudo apt --assume-yes install boinc-client
+/etc/init.d/boinc-client start
+boinccmd --create_account http://climateprediction.net/ gian.frei@lernende.bbw.ch bbwadmin bbwadmin
+boinccmd --project_attach http://climateprediction.net/ bbwadmin bbwadmin
+boinccmd --get_state
